2bdbca198cd21cefe3f6d51f127efd9cb3579a06
dcrlsn <dcarlson@viimed.com>
2024-02-22 20:48:00 -0500

    woo

---
+++ go-webdev/go-warmup/act1/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type square struct {
+	side float64
+}
+
+type circle struct {
+	radius float64
+}
+
+func (s square) area() float64 {
+	return s.side * s.side
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+type shape interface {
+	area() float64
+}
+
+func info(s shape) {
+	fmt.Println(s.area())
+}
+
+func main() {
+	s := square{side: 2}
+	c := circle{radius: 3}
+	info(s)
+	info(c)
+}
